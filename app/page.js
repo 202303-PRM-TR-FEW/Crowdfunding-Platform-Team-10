6d4879aea09ff636dd689eeeabcb267b49c619cb
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-07-04 14:43:51 +0300

    Feat: added validation

--- app/page.js
+++ app/page.js
@@ -26,11 +26,18 @@ export default function Home() {
   }, []);
 
   const propro = projects.map((pro) => {
-    return  <div key={pro.id}>
-      {pro.title}
-      <img src={pro.image} width={200} height={200} alt={pro.title} />
-    </div>;
+    return (
+      <div key={pro.id}>
+        {pro.title}
+        <img src={pro.image} width={200} height={200} alt={pro.title} />
+      </div>
+    );
   });
+  const [open, setOpen] = useState(false);
+
+  const handleNewProject = () => {
+    open === false ? setOpen(true) : setOpen(false);
+  };
 
   return <>{propro}</>;
 }
