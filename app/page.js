f88cda9af0452be61ced7eea1f8aef17a5edcd11
kainy01 <kainykawai@gmail.com>
2023-07-05 02:52:42 +0300

    Auth working with protacted page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,45 @@
 "use client";
 
+import React, { useState } from "react";
+import ProjectForm from "./components/ProjectForm";
+import DonationForm from "./components/DonationForm";
+
 const Home = () => {
-  return <div>Im the home page</div>;
+  const [openProjectForm, setOpenProjectForm] = useState(false);
+  const [openDonationForm, setOpenDonationForm] = useState(false);
+
+  const handleNewProject = () => {
+    openProjectForm === false
+      ? setOpenProjectForm(true)
+      : setOpenProjectForm(false);
+  };
+  const handleDonationForm = () => {
+    openDonationForm === false
+      ? setOpenDonationForm(true)
+      : setOpenDonationForm(false);
+  };
+
+  return (
+    <div>
+      Home
+      <br />
+      <button className="p-4 border-2" onClick={handleNewProject}>
+        New Project
+      </button>
+      <button className="p-4 border-2" onClick={handleDonationForm}>
+        Donation
+      </button>
+      <ProjectForm
+        openProjectForm={openProjectForm}
+        setOpenProjectForm={setOpenProjectForm}
+      />
+      <DonationForm
+        openDonationForm={openDonationForm}
+        setOpenDonationForm={setOpenDonationForm}
+      />
+    </div>
+  );
+
 };
 
 export default Home;
