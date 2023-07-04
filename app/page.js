fcf448ceef0247381fd5a64ec2a3c5a9089ce5ee
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-07-04 17:36:23 +0300

    Feat: donation form created

--- app/page.js
+++ app/page.js
@@ -1,12 +1,21 @@
 "use client";
 import React, { useState } from "react";
 import ProjectForm from "./components/ProjectForm";
+import DonationForm from "./components/DonationForm";
+
 const Home = () => {
   const [openProjectForm, setOpenProjectForm] = useState(false);
+  const [openDonationForm, setOpenDonationForm] = useState(false);
+
   const handleNewProject = () => {
     openProjectForm === false
       ? setOpenProjectForm(true)
       : setOpenProjectForm(false);
+  };
+  const handleDonationForm = () => {
+    openDonationForm === false
+      ? setOpenDonationForm(true)
+      : setOpenDonationForm(false);
   };
 
   return (
@@ -16,9 +25,16 @@ const Home = () => {
       <button className="p-4 border-2" onClick={handleNewProject}>
         New Project
       </button>
+      <button className="p-4 border-2" onClick={handleDonationForm}>
+        Donation
+      </button>
       <ProjectForm
         openProjectForm={openProjectForm}
         setOpenProjectForm={setOpenProjectForm}
+      />
+      <DonationForm
+        openDonationForm={openDonationForm}
+        setOpenDonationForm={setOpenDonationForm}
       />
     </div>
   );
