39ca3264404ac28076faf1da087a79df238ac391
kainy01 <kainykawai@gmail.com>
2023-07-05 15:30:09 +0300

    fixed

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import "./globals.css";
-// import { Navbar } from "./components/Navbar";
 import { Inter } from "next/font/google";
-// import { useRouter } from "next/navigation";
+
 import { AuthContextProvider } from "./context/AuthContext";
 import { FundProvider } from "./context/FundContext";
 import Topbar from "./components/Topbar";
@@ -14,30 +13,16 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  // const router = useRouter();
-
-  // Redirect to login if user is not authenticated
-  // if (router.pathname === "/profile") {
-  //   router.push("/login");
-  //   return null;
-  // }
-
   return (
     <html lang="en">
       <FundProvider>
         <body className={inter.className}>
           <AuthContextProvider>
-            <Navbar />
+            <Topbar />
             {children}
           </AuthContextProvider>
         </body>
       </FundProvider>
-      <body className={inter.className}>
-        <AuthContextProvider>
-          <Topbar />
-          {children}
-        </AuthContextProvider>
-      </body>
     </html>
   );
 }
