59c2a61d5552c8056674c1af83c359cc859e9639
kainy01 <kainykawai@gmail.com>
2023-07-08 19:23:03 +0300

    Feat: user can add info and when he create a project his name n id saves also

--- app/config/firebase.js
+++ app/config/firebase.js
@@ -14,5 +14,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
-export const  db = getFirestore();
+export const  db = getFirestore(app);
 export default app
