8590b91170de39691ac64992cc70bfa117c3c4f7
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-07-09 12:51:53 +0300

    added validation to login form

--- app/login/page.js
+++ app/login/page.js
@@ -1,80 +1,64 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import React, { useState } from "react";
-import { useAuth } from "../context/AuthContext";
-import Link from "next/link";
+import LoginForm from "@/components/forms/LoginForm";
+
+// import { useRouter } from "next/navigation";
+// import { useAuth } from "../context/AuthContext";
+// import Link from "next/link";
+// import {
+//   CardHeader,
+//   CardBody,
+//   CardFooter,
+//   Typography,
+//   Input,
+//   Button,
+//   Card,
+// } from "@material-tailwind/react";
+// import { InformationCircleIcon } from "@heroicons/react/24/solid";
+
+// import { useForm } from "react-hook-form";
+// import { yupResolver } from "@hookform/resolvers/yup";
+// import * as yup from "yup";
+// const schema = yup
+//   .object({
+//     email: yup
+//       .string()
+//       .required("Email is Required !")
+//       .email("Use a Valid Email"),
+//     password: yup.string().required("Password is Required !"),
+//   })
+//   .required();
 
 const Login = () => {
-  const inputStyle =
-    "shadow appearance-none border border-grey-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline";
-  const formStyle = "w-full max-w-sm m-10";
+  // const {
+  //   register,
+  //   handleSubmit,
 
-  const router = useRouter();
-  const { user, login } = useAuth();
-  const [data, setData] = useState({
-    email: "",
-    password: "",
-  });
+  //   formState: { errors },
+  // } = useForm({
+  //   defaultValues: {
+  //     email: "",
+  //     password: "",
+  //   },
+  //   resolver: yupResolver(schema),
+  // });
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
+  // const router = useRouter();
+  // const { user, login } = useAuth();
 
-    try {
-      await login(data.email, data.password);
-      router.push("/profile");
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  // const onSubmit = async (data) => {
+  //   console.log(data.email, data.password);
+  //   try {
+  //     await login(data.email, data.password);
+  //     router.push("/profile");
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
 
   return (
-    <div>
-      <form onSubmit={handleLogin} className={formStyle}>
-        <h1 className="text-lg my-5">Login</h1>
-        <div>
-          <label>Email address</label>
-          <input
-            className={inputStyle}
-            onChange={(e) =>
-              setData({
-                ...data,
-                email: e.target.value,
-              })
-            }
-            value={data.email}
-            required
-            type="email"
-            placeholder="Enter email"
-          />
-        </div>
-
-        <div>
-          <label>Password</label>
-          <input
-            className={inputStyle}
-            onChange={(e) =>
-              setData({
-                ...data,
-                password: e.target.value,
-              })
-            }
-            value={data.password}
-            required
-            type="password"
-            placeholder="Password"
-          />
-        </div>
-        <button
-          type="submit"
-          className="shadow bg-blue-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white my-2 py-1 px-2 rounded"
-        >
-          Login
-        </button>
-      <p>Dont have an account? 
-        <Link href="/signup" className="underline">Sign up</Link>
-      </p>
-      </form>
+    <div className="flex justify-center items-center">
+      <LoginForm />
     </div>
   );
 };
