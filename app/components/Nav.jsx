df74d845d44fae6eff8c7ef6503eaa9f2e61e62b
kainy01 <kainykawai@gmail.com>
2023-07-19 18:06:01 +0300

    Added new navbar and currentUser state in Auth context

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,72 +1,51 @@
 "use client";
+import { useState } from "react";
 import * as React from "react";
 import { styled, alpha } from "@mui/material/styles";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
-import AccountCircle from "@mui/icons-material/AccountCircle";
-import MoreIcon from "@mui/icons-material/MoreVert";
+import { collection, onSnapshot, query } from "firebase/firestore";
+import { db } from "@/config/firebase";
 import { useAuth } from "@/context/AuthContext";
-
-import { Button, ListItemIcon, Stack } from "@mui/material";
+import { useRouter } from "next/navigation";
+import SearchList from "./search/SearchList";
+import { Avatar, Container } from "@mui/material";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Box,
+  Menu,
+  MenuItem,
+  ListItemIcon,
+} from "@mui/material";
+import {
+  AccountCircle,
   Dashboard,
   Login,
   Logout,
   Person,
   PersonAdd,
   Settings,
+  MoreVert as MoreIcon,
 } from "@mui/icons-material";
+
 import Link from "next/link";
 
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.5),
+const TransparentAppBar = styled(AppBar)(({ theme }) => ({
+  background: "transparent",
+  boxShadow: "none",
+  backdropFilter: "blur(6px)",
+  WebkitBackdropFilter: "blur(6px)",
+  transition: "background-color 0.3s ease-in-out",
   "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(3),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "#026d5b",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
+    background: alpha(theme.palette.common.white, 0.1),
   },
 }));
 
 export default function Nav() {
-  const { user } = useAuth();
+  const { user, logout, currentUser } = useAuth();
+  const Router = useRouter();
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
@@ -84,6 +63,12 @@ export default function Nav() {
   const handleMenuClose = () => {
     setAnchorEl(null);
     handleMobileMenuClose();
+  };
+  const handleLogout = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+    logout();
+    Router.push("/login");
   };
 
   const handleMobileMenuOpen = (event) => {
@@ -106,6 +91,7 @@ export default function Nav() {
       }}
       open={isMenuOpen}
       onClose={handleMenuClose}
+      color="#00c1a2"
     >
       {user ? (
         <div>
@@ -126,7 +112,7 @@ export default function Nav() {
               Settings
             </MenuItem>
           </Link>
-          <MenuItem onClick={handleMenuClose}>
+          <MenuItem onClick={handleLogout}>
             <ListItemIcon>
               <Logout fontSize="small" />
             </ListItemIcon>
@@ -149,6 +135,7 @@ export default function Nav() {
   const mobileMenuId = "primary-search-account-menu-mobile";
   const renderMobileMenu = (
     <Menu
+      color="#00c1a2"
       anchorEl={mobileMoreAnchorEl}
       anchorOrigin={{
         vertical: "top",
@@ -216,80 +203,141 @@ export default function Nav() {
   );
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" style={{ background: "#00c1a2" }}>
-        <Toolbar>
-          <Link href="/">
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{ display: { xs: "none", sm: "block" } }}
-            >
-              LOGO
-            </Typography>
-          </Link>
+    <Container>
+      <Box sx={{ flexGrow: 1 }}>
+        <TransparentAppBar position="static">
+          <Toolbar>
+            <Link href="/" sx={{ display: { xs: "none", sm: "block" } }}>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ color: "#00c1a2" }}
+              >
+                LOGO
+              </Typography>
+            </Link>
 
-          <Box sx={{ flexGrow: 1 }} />
+            <Box sx={{ display: { xs: "none", md: "block" }, flexGrow: 1 }} />
+            <Box>
+              <SearchComponent />
+            </Box>
 
-          <Search sx={{ flexGrow: 1 }}>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ "aria-label": "search" }}
-            />
-          </Search>
-          <Box sx={{ flexGrow: 1 }} />
-          <Box sx={{ display: { xs: "none", md: "block" }, px: 5 }}>
-            <Stack spacing={{ xs: 1, sm: 2 }} direction="row" useFlexGap>
+            <Box sx={{ display: { xs: "none", md: "block" }, flexGrow: 1 }} />
+            <Box>
               <Link href="/projects">
-                <Typography
-                  variant="h6"
-                  component="div"
-                  sx={{ display: { xs: "none", sm: "block" } }}
-                >
-                  {" "}
-                  Projects
-                </Typography>
+                <ListItemIcon>
+                  <HomeRoundedIcon fontSize="large" sx={{ color: "#00c1a2" }} />
+                </ListItemIcon>
               </Link>
-              {user ? (
-                <Button sx={{ color: "#00937a", mx: 5, bgcolor: "#fff" }}>
-                  Add New Project
-                </Button>
-              ) : null}
-            </Stack>
-          </Box>
-          <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
+            </Box>
+
+            <Box sx={{ display: { xs: "none", md: "flex" } }}>
+              <IconButton
+                size="large"
+                edge="end"
+                aria-label="account of current user"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={handleProfileMenuOpen}
+                color="inherit"
+                sx={{ color: "#00c1a2" }}
+              >
+                {currentUser ? (
+                  <Avatar alt={currentUser.name} src={currentUser.userImg} />
+                ) : (
+                  <AccountCircle fontSize="large" />
+                )}
+              </IconButton>
+            </Box>
+            <Box sx={{ display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="show more"
+                aria-controls={mobileMenuId}
+                aria-haspopup="true"
+                onClick={handleMobileMenuOpen}
+                color="inherit"
+                sx={{ color: "#00c1a2" }}
+              >
+                <MoreIcon />
+              </IconButton>
+            </Box>
+          </Toolbar>
+        </TransparentAppBar>
+        {renderMobileMenu}
+        {renderMenu}
+      </Box>
+    </Container>
+  );
+}
+
+function SearchComponent() {
+  const [searchProjects, setSearchProjects] = useState();
+
+  const handleSearch = (e) => {
+    const q = query(collection(db, "projects"));
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      let projectsArr = [];
+      QuerySnapshot.forEach((doc) => {
+        projectsArr.push({ ...doc.data(), id: doc.id });
+      });
+      if (e.target.value !== "") {
+        setSearchProjects(
+          projectsArr.filter((project) =>
+            project.name.toLowerCase().includes(e.target.value.toLowerCase())
+          )
+        );
+      } else {
+        setSearchProjects();
+      }
+    });
+  };
+
+  const [values, setValues] = useState();
+  const handleClick = () => {
+    setSearchProjects();
+    setValues("");
+  };
+  return (
+    <div className="relative flex w-max mx-2">
+      <input
+        onChange={handleSearch}
+        onClick={() => setValues()}
+        type="search"
+        label="Search for projects"
+        value={values}
+        placeholder="Search..."
+        className="min-w-[100px] lg:w-[350px] border  rounded-full pr-16 pl-4 py-2 bg-gray-100 text-[#00c1a2] focus:outline-none focus:ring-1 focus:ring-[#00c1a2] shadow-md"
+      />
+      <div
+        onClick={handleClick}
+        className={`${
+          !searchProjects ? "hidden" : "flex"
+        } absolute top-12 w-full`}
+      >
+        <SearchList searchProjects={searchProjects} />
+      </div>
+      {/* Search Icon */}
+      <div className="flex items-center">
+        <div className="absolute right-2">
+          <div className="bg-[#00c1a2] py-1 px-4 rounded-full">
+            <svg
+              className="h-5 w-5 text-white pointer-events-none"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              <AccountCircle />
-            </IconButton>
-          </Box>
-          <Box sx={{ display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton>
-          </Box>
-        </Toolbar>
-      </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
-    </Box>
+              {/* White Icon Lines */}
+              <path d="M22 22l-6-6" />
+              <circle cx="10" cy="10" r="7" />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
