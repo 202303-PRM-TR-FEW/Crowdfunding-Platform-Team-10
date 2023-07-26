4a4d4c99bc17f963e03124d5ac3ecb0a75092375
Ayşe Merve Kosova <mervekosova1@hotmail.com>
2023-07-26 21:33:17 +0300

    Feat:Now we can see all the comments

--- app/components/Comments.jsx
+++ app/components/Comments.jsx
@@ -9,17 +9,22 @@ const Comments = () => {
   const { comments } = useAuth();
 
   const allComments = comments.map((comment) => {
-    <div className="bg-lightGreen rounded-xl shadow-lg p-6 mb-4 w-[200px] mx-auto">
-      <div className="flex items-center space-x-4 mb-3">
-        <img
-          className="w-12 h-12 rounded-full"
-          src={comment.userImg}
-          alt="User Image"
-        />
-        <p className="font-bold text-black text-xl">{comment.userName}</p>
+    return (
+      <div
+        key={comment.text}
+        className="bg-lightGreen rounded-xl shadow-lg p-6 mb-4 w-[200px] mx-auto"
+      >
+        <div className="flex items-center space-x-4 mb-3">
+          <img
+            className="w-12 h-12 rounded-full"
+            src={comment.userImg}
+            alt="User Image"
+          />
+          <p className="font-bold text-black text-xl">{comment.userName}</p>
+        </div>
+        <p className="text-black text-base">{comment.text}</p>
       </div>
-      <p className="text-black text-base">{comment.text}</p>
-    </div>;
+    );
   });
 
   return (
