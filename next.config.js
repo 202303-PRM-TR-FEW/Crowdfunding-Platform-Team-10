8a0bc4cfbf198997ae08414cc5d8ae2884246aa3
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-08-07 14:12:17 +0300

    Fix: our team github icons fixed

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
         hostname: "firebasestorage.googleapis.com",
       },
     ],
