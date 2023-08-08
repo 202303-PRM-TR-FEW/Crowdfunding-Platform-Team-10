6b0bcd6063212e33725c9bda096ef8ddb49f1b06
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-08-08 14:19:19 +0300

    Feat: newsLetterCard test added

--- app/components/cards/NewsLetterCard.cy.jsx
+++ app/components/cards/NewsLetterCard.cy.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import NewsLetterCard from './NewsLetterCard'
+import React from "react";
+import NewsLetterCard from "./NewsLetterCard";
+import { NextIntlProvider } from "next-intl";
 
-describe('<NewsLetterCard />', () => {
-  it('renders', () => {
+describe("<NewsLetterCard />", () => {
+  const locale = "en";
+  const messages = require(`../../../messages/${locale}.json`);
+
+  it("renders", () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<NewsLetterCard />)
-  })
-})
+    cy.mount(
+      <NextIntlProvider locale={locale} messages={messages}>
+        <NewsLetterCard />,
+      </NextIntlProvider>
+    );
+  });
+});
