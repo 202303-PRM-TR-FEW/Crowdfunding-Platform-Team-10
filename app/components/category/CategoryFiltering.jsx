c5ca40ec98625f5459f548ebcf379490bed6a945
Bal Elsada Hasun <fouadhason@gmail.com>
2023-08-08 19:29:08 +0300

    Feat: Add  ending soon Filter

--- app/components/category/CategoryFiltering.jsx
+++ app/components/category/CategoryFiltering.jsx
@@ -9,7 +9,8 @@ import {
 import React, { useState } from "react";
 IconButton;
 const styles = {
-  main: "flex justify-center md:justify-between items-center gap-2 flex-col md:flex-row ",
+  main:
+    "flex justify-center md:justify-between items-center gap-2 flex-col md:flex-row ",
   header: "header-2 text-lightGreen py-4 text-center md:text-start",
   categoryContainer:
     " px-12 sm:px-0 flex flex-row flex-wrap justify-center gap-3",
@@ -35,15 +36,22 @@ const CategoryFiltering = ({ data, filtrindData }) => {
     if (dropcategory === "All" || dropcategory === "") gruopCat = data;
     else if (dropcategory == "Successful")
       gruopCat = data.filter((project) => project.raised >= project.goal);
-    else if (dropcategory === "Closed") {
+    else if (dropcategory === "Active") {
+      gruopCat = data.filter((project) => {
+        const projectTime = timeStatus(project.endingDate);
+        return projectTime > 0 && project.raised < project.goal;
+      });
+    } else if (dropcategory === "Ending soon") {
+      gruopCat = data.filter((project) => {
+        const projectTime = timeStatus(project.endingDate);
+        return (
+          projectTime <= 5 && projectTime > 0 && project.raised < project.goal
+        );
+      });
+    } else if (dropcategory === "Closed") {
       gruopCat = data.filter((project) => {
         const projectTime = timeStatus(project.endingDate);
         return projectTime <= 0 && project.raised < project.goal;
-      });
-    } else if (dropcategory === "Active") {
-      gruopCat = data.filter((project) => {
-        const projectTime = timeStatus(project.endingDate);
-        return projectTime >= 0 && project.raised < project.goal;
       });
     }
     setActiveCategory(CATEGORY.find((item) => item.id === category).id);
@@ -167,11 +175,13 @@ const CATEGORY = [
     },
   },
 ];
+
 const GROUP_CATEGORY = [
   { id: 1, name: "All", value: "All" },
   { id: 2, name: "Successful", value: "Successful" },
   { id: 3, name: "Active", value: "Active" },
-  { id: 4, name: "Closed", value: "Closed" },
+  { id: 4, name: "Ending soon", value: "Ending soon" },
+  { id: 5, name: "Closed", value: "Closed" },
 ];
 function timeStatus(endingDate) {
   const endDate = new Date(endingDate);
