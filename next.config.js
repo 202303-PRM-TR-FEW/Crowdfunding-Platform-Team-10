c0824708462ee6f80293eb09b56d3f2e7891295d
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-08-08 16:27:14 +0300

    Fix: nextConfig updated

--- next.config.js
+++ next.config.js
@@ -14,6 +14,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "media.sproutsocial.com",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
     ],
   },
 };
