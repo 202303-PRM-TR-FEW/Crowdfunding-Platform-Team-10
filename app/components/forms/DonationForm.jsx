609607cdcf3ff183a906052e7adc2bc1a3f4cbf3
Ayşe Merve Kosova <mervekosova1@hotmail.com>
2023-08-08 18:11:41 +0300

    Fix:typo error fixed(hope its working now)

--- app/components/forms/DonationForm.jsx
+++ app/components/forms/DonationForm.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { Alert, Checkbox, IconButton, Input, Typography } from "@mui/material";
+import { Alert, Checkbox, IconButton, Input } from "@mui/material";
 import { Controller, useForm } from "react-hook-form";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -25,20 +25,21 @@ import { db } from "@/config/firebase";
 import Typography from "@mui/material/Typography";
 import Dialog from "@mui/material/Dialog";
 
-const schema = yup
-  .object({
-    donation: yup
-      .number()
-      .min(1, "Donation Amount Can't Be Lower Than 1 Dollar !")
-      .typeError("Donation Amount Must Be a Number !")
-      .required("Donation Amount is Required !"),
-  })
-  .required();
-
 const DonationForm = ({ openDonationForm, setOpenDonationForm, id, title }) => {
   const { user } = useAuth();
   const t = useTranslations("DonationForm");
   const [currentUser, setCurrentUser] = useState();
+
+  const schema = yup
+    .object({
+      donation: yup
+        .number()
+        .min(1, `${t("donation-amount-msg")}`)
+        .typeError(`${t("donation-amount-two-msg")}`)
+        .required(`${t("donation-required-msg")}`),
+    })
+    .required();
+
   useEffect(() => {
     if (user && user.email) {
       const q = query(
