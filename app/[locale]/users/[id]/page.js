96c78122d1b8ed444db701a589b021d17d96e240
kainy01 <kainykawai@gmail.com>
2023-08-09 20:59:55 +0300

    fef

--- app/[locale]/users/[id]/page.js
+++ app/[locale]/users/[id]/page.js
@@ -1,43 +1,33 @@
 "use client";
-
 import React, { useEffect, useState } from "react";
-
 import Link from "next-intl/link";
-
 import LoaderStyle from "@/components/helper/LoaderStyle";
-
 import { useAuth } from "@/context/AuthContext";
-
 import { useTranslations } from "next-intl";
 import { Box } from "@mui/system";
 import SummaryCard from "@/components/cards/SummaryCard";
+import Image from "next/image";
 
 function Page({ params }) {
-  console.log(params);
-  const { user, loading, projects, setLoading } = useAuth();
+  const { loading, projects, setLoading } = useAuth();
   const [usersProjects, setUsersProjects] = useState([]);
+
   const t = useTranslations("Cards");
 
   useEffect(() => {
-    setTimeout(() => {
-      if (user === null) {
-        // router.push("/login");
-        console.log(user);
-      } else {
-        setLoading(true);
+    setLoading(true);
+    const projectArray = Object.values(projects);
+    const projectWithUser = projectArray.filter(
+      (project) => project.creator.userId === params.id
+    );
+    setUsersProjects(projectWithUser);
+    setLoading(false);
+  }, []);
 
-        const projectArray = Object.values(projects);
-        const projectWithUser = projectArray.filter(
-          (project) => project.creator.userId === params.id
-        );
-        setUsersProjects(projectWithUser);
-        setLoading(false);
-        console.log(user);
-      }
-    }, 600);
-  }, [projects]);
+  const user = usersProjects.length > 0 ? usersProjects[0].creator : null;
+  const numProjects = usersProjects.length;
 
-  const allProjects = projects ? (
+  const allProjects = usersProjects ? (
     usersProjects.length > 0 ? (
       usersProjects.map((card) => {
         return (
@@ -75,7 +65,24 @@ function Page({ params }) {
   }
   return (
     <div className=" bg-gradient-to-t from-transparent to-teal-50 relative overflow-hidden ">
-      <div className="container mx-auto py-28">{allProjects}</div>
+      <div className="container mx-auto py-28 text-center">
+        {user && (
+          <div className="mb-6">
+            <Image
+              src={user.userImg}
+              alt={`${user.name}'s Profile`}
+              className="w-20 h-20 rounded-full mx-auto mb-2"
+              width={100}
+              height={100}
+            />
+            <h1 className="text-2xl font-semibold">{user.name}</h1>
+            <p className="text-gray-500">{numProjects} Projects</p>
+          </div>
+        )}
+        <div className=" items-start justify-around mx-auto flex flex-wrap gap-3 mt-2 ">
+          {allProjects}
+        </div>
+      </div>
     </div>
   );
 }
