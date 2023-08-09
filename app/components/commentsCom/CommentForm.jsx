a7d6229b2acdbebb02c30b44278ef1cea481e296
Ayşe Merve Kosova <mervekosova1@hotmail.com>
2023-08-09 13:57:02 +0300

    Fix:problems related currentUser hopefully solved

--- app/components/commentsCom/CommentForm.jsx
+++ app/components/commentsCom/CommentForm.jsx
@@ -1,6 +1,5 @@
-"use client";
-import React, { useState } from "react";
-import { collection, addDoc } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { collection, addDoc, getDocs, query, where } from "firebase/firestore";
 
 import { useAuth } from "@/context/AuthContext";
 import { db } from "@/config/firebase";
@@ -12,8 +11,36 @@ const CommentForm = ({ params }) => {
   const [commentText, setCommentText] = useState("");
   const t = useTranslations("CommentForm");
 
-  const { currentUser } = useAuth();
+  const { user } = useAuth();
+  const [currentUser, setCurrentUser] = useState();
 
+  useEffect(() => {
+    if (user && user.email) {
+      const q = query(
+        collection(db, "users"),
+        where("email", "==", user.email)
+      );
+
+      const fetchUserData = async () => {
+        try {
+          const querySnapshot = await getDocs(q);
+
+          if (!querySnapshot.empty) {
+            const userData = querySnapshot.docs[0].data();
+
+            setCurrentUser({ ...userData, id: user.uid });
+          } else {
+            console.log("no data matched");
+            setCurrentUser({});
+          }
+        } catch (error) {
+          console.error("Error fetching user data: ", error);
+        }
+      };
+
+      fetchUserData();
+    }
+  }, [user]);
   const handleFormSubmit = async (event) => {
     event.preventDefault();
     try {
@@ -34,26 +61,26 @@ const CommentForm = ({ params }) => {
     }
   };
   return (
-    <div className="mt-5">
-      <div className="title flex flex-col gap-3 ">
-        <h3 className="text-base font-bold text-lightGreen">
+    <div className="mt-5 w-full">
+      <div className="title flex flex-col gap-3 w-full">
+        <h3 className="text-base font-bold text-lightGreen w-full">
           {t("leave-com")}
         </h3>
       </div>
-      <form className="flex flex-col gap-4" onSubmit={handleFormSubmit}>
-        <div className="mt-3">
+      <form className="flex flex-col gap-4 w-full" onSubmit={handleFormSubmit}>
+        <div className="mt-3 w-full">
           <textarea
             label={t("message")}
             placeholder={t("text")}
             type="text"
             value={commentText}
             onChange={(e) => setCommentText(e.target.value)}
-            className="border-[#00c1a2] border-2 w-full   p-2 rounded-lg"
+            className="border-[#00c1a2] border-2 w-full p-2 rounded-lg"
             rows="4"
             required
           />
         </div>
-        <button className="btn-primary w-full"> {t("btn-submit")}</button>
+        <button className="btn-primary w-full">{t("btn-submit")}</button>
       </form>
     </div>
   );
