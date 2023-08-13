1099f59caca2057c32f96b33bd92d29a1391b28b
kainy01 <kainykawai@gmail.com>
2023-08-13 20:47:56 +0300

    fixed

--- app/components/helper/UserNameImg.jsx
+++ app/components/helper/UserNameImg.jsx
@@ -9,19 +9,16 @@ function UserNameImg({ userImg, userName, userId }) {
   const t = useTranslations("Helper");
   return (
     <div className="flex items-center justify-start gap-3 ">
-      {userId ? (
-        <Link href={`/users/${userId}`}>
-          <div className="w-[45px]">
-            {userImg ? (
-              <Avatar alt={userName} src={userImg} />
-            ) : (
-              <AccountCircle fontSize="large" />
-            )}
-          </div>
-        </Link>
-      ) : (
-        <LoaderStyle />
-      )}
+      <Link href={`/users/${userId}`}>
+        <div className="w-[45px]">
+          {userImg ? (
+            <Avatar alt={userName} src={userImg} />
+          ) : (
+            <AccountCircle fontSize="large" />
+          )}
+        </div>
+      </Link>
+
       <div>
         <p className="text-md color-yellow">{userName} </p>
         <p className="text-blue-gray-400 text-xs">{t("organize")}</p>
